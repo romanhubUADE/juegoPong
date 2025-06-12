68d3bd81a2224bf7630ed33654785b6711608795
roman <romsanchez@uade.edu.ar>
2025-06-12 16:42:04 -0300

    Arreglo altura pantalla

--- src/main/Constantes.java
+++ src/main/Constantes.java
@@ -2,6 +2,6 @@ package main;
 
 public class Constantes {
     public static final int ANCHO_PANTALLA = 800;
-    public static final int ALTO_PANTALLA = 400;
+    public static final int ALTO_PANTALLA = 800;
     public static final String TITULO_PANTALLA = "PONG";
 }
